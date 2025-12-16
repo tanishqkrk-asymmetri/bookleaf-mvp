11c49db43335193ba1b88a8a8d9de4c7ae6f9a5b
Tanishq Kaushal <tanishqkrk@gmail.com>
2025-12-16 16:14:11 +0530

    Updates

--- app/api/searchImage/route.ts
+++ app/api/searchImage/route.ts
@@ -2,7 +2,10 @@ export async function POST(request: Request) {
   const { query, page } = await request.json();
   const response = await (
     await fetch(
-      `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${process.env.UNSPLASH_ACCESS_KEY}`
+      `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${
+        process.env.UNSPLASH_ACCESS_KEY ||
+        "Wwx4sQ2YJcuBYIJhI8ORpypZoAjF6OdyoZfjcTddlUI"
+      }`
     )
   ).json();
   return Response.json({ response });
