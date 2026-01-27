ac4fe856316d6f99f799bb7825e8903091df6a27
Tanishq Kaushal <tanishqkrk@gmail.com>
2026-01-27 14:04:43 +0530

    Updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -140,7 +140,7 @@ export default function Header({ url }: { url?: string }) {
       console.log(post.body);
 
       console.log(url);
-      window.location.href = url || "/";
+      // window.location.href = url || "/";
     } catch (error) {
       console.error("Error uploading:", error);
     } finally {
